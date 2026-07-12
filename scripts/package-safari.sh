ca0bd6b183d5442fc149db83e604e0f0dc3fa096
Bbrizly <kingofdeath2533@yahoo.com>
2026-07-12 19:07:11 -0300

    Align Safari extension signing settings

--- scripts/package-safari.sh
+++ scripts/package-safari.sh
@@ -5,6 +5,7 @@ ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 OUTPUT="${1:-$ROOT/safari-app}"
 BUNDLE_ID="${BUNDLE_ID:-com.bbrizly.pluck}"
 APP_NAME="${APP_NAME:-Pluck Pins}"
+DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM:-69NPZWZB47}"
 
 if xcrun --find safari-web-extension-packager >/dev/null 2>&1; then
   TOOL="safari-web-extension-packager"
@@ -27,6 +28,19 @@ echo "Safari Xcode project created in: $OUTPUT"
 PROJECT=$(find "$OUTPUT" -maxdepth 2 -name '*.xcodeproj' -print -quit 2>/dev/null || true)
 if [[ -n "$PROJECT" && "$APP_NAME" == "Pluck Pins" ]]; then
   TARGET_BUNDLE_ID="$BUNDLE_ID" /usr/bin/perl -0pi -e 's/PRODUCT_BUNDLE_IDENTIFIER = "?com\.bbrizly\.Pluck-Pins"?;/PRODUCT_BUNDLE_IDENTIFIER = $ENV{TARGET_BUNDLE_ID};/g' "$PROJECT/project.pbxproj"
+  DEVELOPMENT_TEAM="$DEVELOPMENT_TEAM" /usr/bin/perl -0pi -e '
+    sub fix_signing {
+      my ($block) = @_;
+      return $block unless $block =~ /PRODUCT_BUNDLE_IDENTIFIER = com\.bbrizly\.pluck(?:\.Extension)?;/;
+      my $team = $ENV{DEVELOPMENT_TEAM};
+      $block =~ s/DEVELOPMENT_TEAM = [^;]+;/DEVELOPMENT_TEAM = $team;/g;
+      $block =~ s/"CODE_SIGN_IDENTITY\[sdk=macosx\*\]" = "[^"]+";/"CODE_SIGN_IDENTITY[sdk=macosx*]" = "Apple Development";/g;
+      $block =~ s/(CODE_SIGN_STYLE = Automatic;\n)/\t\t\t\t"CODE_SIGN_IDENTITY[sdk=macosx*]" = "Apple Development";\n$1/ unless $block =~ /CODE_SIGN_IDENTITY\[sdk=macosx\*\]/;
+      $block =~ s/(CURRENT_PROJECT_VERSION = [^;]+;\n)/$1\t\t\t\tDEVELOPMENT_TEAM = $team;\n/ unless $block =~ /DEVELOPMENT_TEAM = /;
+      return $block;
+    }
+    s/(buildSettings = \{.*?\n\t\t\t\};)/fix_signing($1)/ges;
+  ' "$PROJECT/project.pbxproj"
   RESOURCE_ROOT="$ROOT/extension" \
   RESOURCE_ROOT_NO_SLASH="${ROOT#/}/extension" \
   TARGET_RESOURCE_PATH="../../../extension" \
